52cab72ecf760c5567207bd91419e9cf99aafd52
Rajat Khandelwal <rajatgupta59@gmail.com>
2015-05-11 19:14:45 +0530

    created persistent jira and rbt client objects

---
+++ post-review.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Modified based on Kafka's patch review tool
+
+# Required Modules:
+# - python-argparse
+# - python-rbtools
+# - python-setuptools
+# - jira
+# - jira-python
+
+import pickle
+import argparse
+import sys
+import os
+import time
+import datetime
+import tempfile
+from jira.client import JIRA
+from rbtools.api.client import RBClient
+from getpass import getpass
+
+
+def get_post_review_dir():
+    post_review_path = os.path.join(os.getenv('HOME'), ".post-review")
+    if not os.path.exists(post_review_path):
+        os.mkdir(post_review_path)
+    return post_review_path
+
+def load_jira_to_rbt_map():
+    return {}
+
+def get_jira():
+    options = {
+        'server': 'https://issues.apache.org/jira'
+    }
+    # read the config file
+    post_review_path = get_post_review_dir()
+    jira_path = os.path.join(post_review_path, "jira")
+    if os.path.exists(jira_path):
+        with open(jira_path) as jira_file:
+            try:
+                jira = pickle.load(jira_file)
+                jira._session.max_retries = 3
+                if jira.session():
+                    return jira
+            except:
+                pass
+    username = raw_input("Enter JIRA Username: ")
+    password = getpass("Enter password: ")
+    jira = JIRA(options, basic_auth=(username, password))
+    with open(jira_path, 'wb') as jira_file:
+        pickle.dump(jira, jira_file)
+    return jira
+
+
+def get_rbclient():
+    with open(".reviewboardrc") as reviewboardrc:
+        options = dict(map(lambda x: x.replace('"','').strip(), line.strip().split('=')) for line in reviewboardrc)
+    rbclient = RBClient(options['REVIEWBOARD_URL'].replace('https', 'http'))
+    if rbclient.get_root().get_session()['authenticated']:
+        return rbclient
+    username = raw_input("Enter review board Username: ")
+    password = getpass("Enter password: ")
+    rbclient.login(username, password)
+    return rbclient
+
+
+def main():
+    ''' main(), shut up, pylint '''
+    jira_to_rbt_map = load_jira_to_rbt_map()
+    jira = get_jira()
+    rbclient = get_rbclient().get_root()
+    popt = argparse.ArgumentParser(description='Apache Lens patch review tool')
+    popt.add_argument('-b', '--branch', action='store', dest='branch', required=True,
+                      help='Tracking branch to create diff against', default="master")
+    popt.add_argument('-j', '--jira', action='store', dest='jira', required=True,
+                      help='JIRA corresponding to the reviewboard')
+    popt.add_argument('-s', '--summary', action='store', dest='summary', required=False,
+                      help='Summary for the reviewboard')
+    popt.add_argument('-d', '--description', action='store', dest='description', required=False,
+                      help='Description for reviewboard')
+    popt.add_argument('-r', '--rb', action='store', dest='reviewboard', required=False,
+                      help='Review board that needs to be updated')
+    popt.add_argument('-t', '--testing-done', action='store', dest='testing', required=False,
+                      help='Text for the Testing Done section of the reviewboard')
+    popt.add_argument('-db', '--debug', action='store_true', required=False, help='Enable debug mode')
+
+    opt = popt.parse_args()
+
+    sys.exit(0)
+    patch_file = tempfile.gettempdir() + "/" + opt.jira + "." + opt.version + ".patch"
+    if opt.reviewboard:
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
+        patch_file = tempfile.gettempdir() + "/" + opt.jira + '_' + st + '.patch'
+
+    os.system("git remote update")
+    os.system("git merge " + opt.branch)
+    os.system("git mergetool")
+    os.system('git commit -am "merge with ' + opt.branch + '"')
+    opt.jira = opt.jira.upper()
+    if not opt.reviewboard:
+        jira_to_rbt_map.get(opt.jira)
+    rbclient.get_review_request(review_request_id=33943).get_diffs()
+    rb_command = "post-review --publish --tracking-branch " + opt.branch + " --target-groups=lens --bugs-closed=" + opt.jira
+    rb_command = rb_command + " --username " + opt.reviewboard_user + " --password " + opt.reviewboard_password
+
+    if opt.debug:
+        rb_command = rb_command + " --debug"
+    summary = "Patch for " + opt.jira
+    if opt.summary:
+        summary = opt.summary
+    rb_command = rb_command + " --summary \"" + summary + "\""
+    if opt.description:
+        rb_command = rb_command + " --description \"" + opt.description + "\""
+    if opt.reviewboard:
+        rb_command = rb_command + " -r " + opt.reviewboard
+    if opt.testing:
+        rb_command = rb_command + " --testing-done=\"" + opt.testing + "\""
+    if opt.debug:
+        print rb_command
+    p = os.popen(rb_command)
+    rb_url = ""
+    for line in p:
+        print line
+        if line.startswith('http'):
+            rb_url = line
+        elif line.startswith("There don't seem to be any diffs"):
+            print 'ERROR: Your reviewboard was not created/updated since there was no diff to upload. The reasons that can cause this issue are 1) Your diff is not checked into your local branch. Please check in the diff to the local branch and retry 2) You are not specifying the local branch name as part of the --branch option. Please specify the remote branch name obtained from git branch -r'
+            p.close()
+            sys.exit(1)
+        elif line.startswith("Your review request still exists, but the diff is not attached") and not opt.debug:
+            print 'ERROR: Your reviewboard was not created/updated. Please run the script with the --debug option to troubleshoot the problem'
+            p.close()
+            sys.exit(1)
+    p.close()
+    if opt.debug:
+        print 'rb url=', rb_url
+
+    git_command = "git diff " + opt.branch + " > " + patch_file
+    if opt.debug:
+        print git_command
+    p = os.popen(git_command)
+    p.close()
+
+    print 'Creating diff against', opt.branch, 'and uploading patch to JIRA', opt.jira
+    jira = get_jira()
+    issue = jira.issue(opt.jira)
+    attachment = open(patch_file)
+    jira.add_attachment(issue, attachment)
+    attachment.close()
+
+    comment = "Created reviewboard "
+    if not opt.reviewboard:
+        print 'Created a new reviewboard ', rb_url
+    else:
+        print 'Updated reviewboard', opt.reviewboard
+        comment = "Updated reviewboard "
+
+    comment = comment + rb_url
+    jira.add_comment(opt.jira, comment)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
+
