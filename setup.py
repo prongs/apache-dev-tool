9cc6ce443c865d06151fe9985a87bf07c0500b62
Rajat Khandelwal <rajatgupta59@gmail.com>
2015-08-14 14:44:48 +0530

    0.1 again

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='apache_dev_tool',
     packages=['apache_dev_tool'],  # this must be the same as the name above
-    version='0.2',
+    version='0.1',
     description='Tool for apache contributors and committers.',
     author='Rajat Khandelwal',
     author_email='rajatgupta59@gmail.com',
     url='https://github.com/prongs/apache_dev_tool',  # use the URL to the github repo
-    download_url='https://github.com/prongs/apache-dev-tool/tarball/0.2',  # I'll explain this in a second
+    download_url='https://github.com/prongs/apache-dev-tool/tarball/0.1',  # I'll explain this in a second
     keywords=['apache', 'open-source', 'reviewboard', 'jira', 'test-patch'],  # arbitrary keywords
     classifiers=[],
     install_requires=[
