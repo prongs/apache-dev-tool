b1e9302b6180a4e573f6a01120273f63dcecba25
Rajat Khandelwal <rajatgupta59@gmail.com>
2015-08-14 15:13:33 +0530

    checkpoint

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from distutils.core import setup
 
 setup(
-    name='apache_dev_tool',
+    name='apache-dev-tool',
     packages=['apache_dev_tool'],  # this must be the same as the name above
-    version='0.0.1',
+    version='0.0.2',
     description='Tool for apache contributors and committers.',
     author='Rajat Khandelwal',
     author_email='rajatgupta59@gmail.com',
     url='https://github.com/prongs/apache_dev_tool',  # use the URL to the github repo
-    download_url='https://github.com/prongs/apache-dev-tool/tarball/0.0.1',  # I'll explain this in a second
+    download_url='https://github.com/prongs/apache-dev-tool/tarball/0.0.2',  # I'll explain this in a second
     keywords=['apache', 'open-source', 'reviewboard', 'jira', 'test-patch'],  # arbitrary keywords
     classifiers=[],
     install_requires=[
         "argparse>=1.3.0",
         "jira>=0.47",
         "beautifulsoup4>=4.3.2",
-        "RBTools>=0.7.4",
+        "RBTools==0.7.4",
         "requests>=2.7.0",
     ],
     entry_points={
