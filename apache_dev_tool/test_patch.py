c94f18a86025e47666a1b18264f5db719436b962
Rajat Khandelwal <rajatgupta59@gmail.com>
2015-08-26 20:14:48 +0530

    test patch enhancements

--- apache_dev_tool/test_patch.py
+++ apache_dev_tool/test_patch.py
@@ -1,22 +1,31 @@
 import os
 
 
+def text_status(status):
+    return "Success" if status == 0 else "Failure"
+
+
 class PatchTester:
     def __init__(self, client, opt):
         self.client = client
         self.opt = opt
         self.issue = self.client.jira_client.issue(self.opt.jira)
-        self.opt.branch = self.opt.branch or self.client.get_branch()
+        self.branch = self.opt.branch or self.client.get_branch()
 
     def test_patch(self):
+        os.system("git reset --hard")
+        os.system("git checkout " + self.branch)
+        os.system("git pull origin " + self.branch)
         attachment = self.client.get_latest_attachment(self.issue)
         status = os.system("curl " + attachment.url + " | git apply")
-        print "apply latest patch status:", status
+        print "apply latest patch status:", text_status(status)
         if status != 0:
             return status
         # status, output = commands.getstatusoutput("mvn clean install")
-        command = "mvn clean install"
+        command = "mvn clean install -DskipTests -DskipCheck"
         status = os.system(command)
-        comment = "Command: " + command + " in Pre Commit Build " + (
-            "Succeeded" if status == 0 else "Failed") + " " + os.getenv("BUILD_URL", "")
+        comment = "Applied patch: [%s|%s] and ran command: %s. " \
+                  "Result: %s. " \
+                  "Build Job: %s" % (attachment.title, attachment.url, command,
+                                     text_status(status), os.getenv("BUILD_URL", "NO url availavle"))
         self.client.jira_client.add_comment(self.issue, comment.strip())
