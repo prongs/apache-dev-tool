20cbe5dd95192a60264d83ebe442cdd8d1f612e7
Rajat Khandelwal <rajatgupta59@gmail.com>
2015-09-10 20:57:55 +0530

    release 0.1.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-version = '0.1.1'
+version = '0.1.2'
 setup(
     name='apache_dev_tool',
     packages=['apache_dev_tool'],  # this must be the same as the name above
